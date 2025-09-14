51835053c0baad52ddc8f7cfeca29a9b48bbe982
josep <joseph@jcooney.net>
2025-09-15 09:03:57 +1000

    additional test

--- Skeleton.Postgres.Tests/PostgresTypeProviderTests.cs
+++ Skeleton.Postgres.Tests/PostgresTypeProviderTests.cs
@@ -105,6 +105,32 @@ public class PostgresTypeProviderTests : DbTestBase
     }
 
     [Fact]
+    public void CanReadTypeInformationForMultipeSchemas()
+    {
+        var testDbInfo = CreateTestDatabase(TestDatbaseMultiSchema);
+        try
+        {
+            var provider = new PostgresTypeProvider(testDbInfo.connectionString);
+            var model = provider.GetDomain(new Settings(new MockFileSystem()));
+            provider.GetOperations(model);
+            model.Operations.Count.ShouldBe(1);
+
+            model.ResultTypes.Count.ShouldBe(1);
+            
+            var op = model.Operations.First();
+            op.Parameters.Count.ShouldBe(3);
+            var customTypeParam = op.Parameters.Single(p => p.Name == "government_area_to_add");
+            customTypeParam.ProviderTypeName.ShouldBe("gov.government_area_new");
+            customTypeParam.ClrType.ShouldBe(typeof(ResultType));
+            model.ResultTypes.Count(t => t.Name == customTypeParam.ProviderTypeName).ShouldBe(1);
+        }
+        finally
+        {
+            DestroyTestDb(testDbInfo.dbName);
+        }
+    }
+
+    [Fact]
     public void CanCreateOperationWhenParameterNameDoesNotMatchFieldOnDomainObject()
     {
         var testDbInfo = CreateTestDatabase(DbSchemaWithFunction);
@@ -332,6 +358,84 @@ AS $$
     ;
 ";
 
+    public const string TestDatbaseMultiSchema = @"
+    CREATE TABLE ""user"" (
+        id serial primary key NOT NULL,
+        ""name"" text NULL,
+        is_system bool NOT NULL,
+        user_name text NOT NULL,
+        created timestamp with time zone not null NOT NULL,
+        created_by int NOT NULL references ""user""(id),
+        CONSTRAINT user_name_unique UNIQUE (user_name)
+    );
+
+    create schema gov;
+
+    create table gov.government_area (
+         id serial primary key not null,
+         name text not null,
+         color char(7),
+         city_town text not null,
+         state varchar(100) not null,
+         country varchar(100) not null,
+         created_by int not null references public.""user""(id),
+        created timestamp with time zone not null,
+        modified_by int references public.""user""(id),
+        modified timestamp with time zone
+    );
+
+    CREATE TYPE gov.government_area_new AS (
+	    ""name"" text,
+        logo_id int4,
+        olor bpchar(7),
+        city_town text,
+        state varchar(100),
+        country varchar(100)
+    );
+
+CREATE OR REPLACE FUNCTION gov.government_area_insert(security_user_id_param integer, created_by integer, government_area_to_add gov.government_area_new)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $$
+    DECLARE new_id integer;
+
+    BEGIN
+
+        IF (security_user_id_param is not null) THEN
+		    perform set_config('app.user_id', security_user_id_param::text, true);
+        END IF;
+
+        insert into government_area (
+            id,
+            ""name"",
+            logo_id,
+            color,
+            city_town,
+            ""state"",
+            country,
+            created_by,
+            created
+        )
+        VALUES
+        (
+            DEFAULT,
+            government_area_to_add.name,
+            government_area_to_add.logo_id,
+            government_area_to_add.color,
+            government_area_to_add.city_town,
+            government_area_to_add.state,
+            government_area_to_add.country,
+            government_area_insert.created_by,
+            clock_timestamp()
+        );
+
+        new_id = currval(pg_get_serial_sequence('government_area', 'id'));
+        return new_id;
+    END
+    $$
+    ;
+";
+    
     public const string DbSchemaWithFunction = @"
 
 create table task_type (
--- Skeleton.Postgres/PostgresTypeProvider.cs
+++ Skeleton.Postgres/PostgresTypeProvider.cs
@@ -1922,8 +1922,7 @@ AND KCU1.TABLE_SCHEMA = '{type.Namespace}'
     FROM pg_catalog.pg_proc p
     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
     left join pg_catalog.pg_description dsc on p.oid = dsc.objoid
-    WHERE pg_catalog.pg_function_is_visible(p.oid)
-    AND n.nspname<> 'pg_catalog'
+    WHERE n.nspname<> 'pg_catalog'
     AND n.nspname<> 'information_schema'
     ORDER BY 1, 2, 4;";
 
